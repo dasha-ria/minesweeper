55d4b30fe9bb69f2b473af3ba431f3fa913a7e44
Daria <dariastoicad@gmail.com>
2023-07-16 21:46:35 +0200

    calculating values of cells

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,24 @@
+import { useState } from "react";
+import { createMinefield } from "./minefield/createMinefield";
+
 function App() {
-  return <></>;
+  const [minefield, setMinefield] = useState(
+    createMinefield({ width: 10, height: 10, mines: 5 })
+  );
+
+  return (
+    <div className="flex flex-col gap-1 items-center justify-center h-screen">
+      {minefield.map((row) => (
+        <div className="flex gap-1">
+          {row.map((cell) => (
+            <div className="flex justify-center items-center  w-12 h-12 border border-black">
+              {cell.value === "Mine" ? "M" : cell.value}
+            </div>
+          ))}
+        </div>
+      ))}
+    </div>
+  );
 }
 
 export default App;
--- src/minefield/createMinefield.ts
+++ src/minefield/createMinefield.ts
@@ -4,6 +4,13 @@ type CreateMinefield = {
   mines: number;
 };
 
+type Cell = {
+  value: string;
+  action: "Reveal" | "Flag" | null;
+};
+
+type Minefield = Array<Array<Cell>>;
+
 function randomiseMineLocations({ width, height, mines }: CreateMinefield) {
   const potentialLocations = [];
 
@@ -36,11 +43,36 @@ function shuffle<T>(array: Array<T>) {
   return array;
 }
 
+function getSurroundingCells({
+  x,
+  y,
+  width,
+  height,
+}: {
+  x: number;
+  y: number;
+  width: number;
+  height: number;
+}) {
+  // prettier-ignore
+  const surroundingCoordinates = [
+    {y: y-1, x: x-1}, {y: y-1, x}, {y: y-1, x: x+1},
+    {y, x: x-1},                   {y, x: x+1},
+    {y: y+1, x: x-1}, {y: y+1, x}, {y: y+1, x: x+1}
+  ]
+
+  return surroundingCoordinates.filter(({ y, x }) => {
+    if (y >= 0 && y < height && x >= 0 && x < width) {
+      return true;
+    }
+  });
+}
+
 export function createMinefield({ width, height, mines }: CreateMinefield) {
   const arr = new Array(height);
 
   for (let i = 0; i < arr.length; i++) {
-    arr[i] = new Array(width);
+    arr[i] = new Array(width).fill({ value: "0" });
   }
 
   //   const mineLocations = [
@@ -51,8 +83,20 @@ export function createMinefield({ width, height, mines }: CreateMinefield) {
   const mineLocations = randomiseMineLocations({ width, height, mines });
 
   mineLocations.forEach(({ x, y }) => {
-    arr[y][x] = { value: "Mine" };
+    arr[y][x] = { value: "Mine", action: null };
   });
 
-  return arr;
+  for (let y = 0; y < height; y++) {
+    for (let x = 0; x < width; x++) {
+      if (arr[y][x].value !== "Mine") {
+        arr[y][x] = {
+          value: getSurroundingCells({ y, x, width, height })
+            .filter((coords) => arr[coords.y][coords.x].value === "Mine")
+            .length.toString(),
+        };
+      }
+    }
+  }
+
+  return arr as Minefield;
 }
